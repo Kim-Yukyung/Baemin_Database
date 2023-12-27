1d47092f16b169c5749eb51c469d89dc4788850c
Kim-Yukyung <154517645+Kim-Yukyung@users.noreply.github.com>
2023-12-27 20:49:56 +0900

    Create menu.sql

---
+++ Dataset/menu.sql
@@ -0,0 +1,250 @@
+INSERT INTO Menu VALUES 
+-- Store 1
+(1, '황금올리브치킨', 20000, 'http://menuImage1.com/150x100.png', 1),
+(2, '극한왕갈비치킨', 23000, 'http://menuImage2.com/150x100.png', 1),
+(3, '스모크치킨', 18000, 'http://menuImage3.com/150x100.png', 1),
+
+-- Store 2
+(4, '페페로니피자', 9900, 'http://menuImage4.com/150x100.png', 2),
+(5, '불고기피자', 11900, 'http://menuImage5.com/150x100.png', 2),
+(6, '스테이크피자', 13900, 'http://menuImage6.com/150x100.png', 2),
+
+-- Store 3
+(7, '엽기떡볶이', 14000, 'http://menuImage7.com/150x100.png', 3),
+(8, '엽기닭볶음탕', 24000, 'http://menuImage8.com/150x100.png', 3),
+(9, '주먹김밥', 2000, 'http://menuImage9.com/150x100.png', 3),
+
+-- Store 4
+(10, '씨푸드킹', 36900, 'http://menuImage10.com/150x100.png', 4),
+(11, 'LA 바비큐 불고기', 29900, 'http://menuImage11.com/150x100.png', 4),
+(12, '베이컨 포테이토', 32500, 'http://menuImage12.com/150x100.png', 4),
+
+-- Store 5
+(13, '고추바사삭', 18000, 'http://menuImage13.com/150x100.png', 5),
+(14, '오븐바사삭', 17000, 'http://menuImage14.com/150x100.png', 5),
+(15, '바게트볼', 3300, 'http://menuImage15.com/150x100.png', 5),
+
+-- Store 6
+(16, '떡볶이', 3500, 'http://menuImage16.com/150x100.png', 6),
+(17, '참치마요컵밥', 4000, 'http://menuImage17.com/150x100.png', 6),
+(18, '모둠튀김', 4500, 'http://menuImage18.com/150x100.png', 6),
+
+-- Store 7
+(19, '슈슈버거', 6000, 'http://menuImage19.com/150x100.png', 7),
+(20, '불고기버거', 4500, 'http://menuImage20.com/150x100.png', 7),
+(21, '빅맥', 5500, 'http://menuImage21.com/150x100.png', 7),
+
+-- Store 8
+(22, '블랙 밀크티', 4500, 'http://menuImage22.com/150x100.png', 8),
+(23, '딸기 쥬얼리 밀크티', 5300, 'http://menuImage23.com/150x100.png', 8),
+(24, '청포토 그린티', 4800, 'http://menuImage24.com/150x100.png', 8),
+
+-- Store 9
+(25, '애플망고치즈설빙', 13900, 'http://menuImage25.com/150x100.png', 9),
+(26, '스윗캔디구슬설빙', 13500, 'http://menuImage26.com/150x100.png', 9),
+(27, '하겐다즈초코설빙', 13500, 'http://menuImage27.com/150x100.png', 9),
+
+-- Store 10
+(28, '모둠보쌈(소)', 36000, 'http://menuImage28.com/150x100.png', 10),
+(29, '보족원쌈(2인)', 49000, 'http://menuImage29.com/150x100.png', 10),
+(30, '커플원쌈', 45000, 'http://menuImage30.com/150x100.png', 10),
+
+-- Store 11
+(31, '소고기쌀국수', 8900, 'http://menuImage31.com/150x100.png', 11),
+(32, '해물쌀국수', 9900, 'http://menuImage32.com/150x100.png', 11),
+(33, '한우곱창쌀국수', 11900, 'http://menuImage33.com/150x100.png', 11),
+
+-- Store 12
+(34, '원조김밥', 3000, 'http://menuImage34.com/150x100.png', 12),
+(35, '참치김밥', 4500, 'http://menuImage35.com/150x100.png', 12),
+(36, '라면', 4500, 'http://menuImage36.com/150x100.png', 12),
+
+-- Store 13
+(37, '쇠고기야채죽', 11500, 'http://menuImage37.com/150x100.png', 13),
+(38, '단호박죽', 10500, 'http://menuImage38.com/150x100.png', 13),
+(39, '전복죽(보양)', 13000, 'http://menuImage39.com/150x100.png', 13),
+
+-- Store 14
+(40, '1인 순살 찜닭', 14000, 'http://menuImage40.com/150x100.png', 14),
+(41, '교동찜닭', 22000, 'http://menuImage41.com/150x100.png', 14),
+(42, '고추장찜닭', 22000, 'http://menuImage42.com/150x100.png', 14),
+
+-- Store 15
+(43, '허니콤보', 23000, 'http://menuImage43.com/150x100.png', 15),
+(44, '교촌오리지날', 19000, 'http://menuImage44.com/150x100.png', 15),
+(45, '반반콤보', 23000, 'http://menuImage45.com/150x100.png', 15),
+
+-- Store 16
+(46, '칠리베이컨 웜볼', 7900, 'http://menuImage46.com/150x100.png', 16),
+(47, '멕시칸 랩 샌드위치', 6700, 'http://menuImage47.com/150x100.png', 16),
+(48, '단호박 크림스프', 3500, 'http://menuImage48.com/150x100.png', 16),
+
+-- Store 17
+(49, '프랭크버거', 4300, 'http://menuImage49.com/150x100.png', 17),
+(50, '쉬림프버거', 4700, 'http://menuImage50.com/150x100.png', 17),
+(51, 'SG불고기버거', 3800, 'http://menuImage51.com/150x100.png', 17),
+
+-- Store 18
+(52, '라면', 3500, 'http://menuImage52.com/150x100.png', 18),
+(53, '바나나', 3000, 'http://menuImage53.com/150x100.png', 18),
+(54, '초코우유', 1800, 'http://menuImage54.com/150x100.png', 18),
+
+-- Store 19
+(55, '샌드위치', 8000, 'http://menuImage55.com/150x100.png', 19),
+(56, '삼각김밥', 1200, 'http://menuImage56.com/150x100.png', 19),
+(57, '오렌지주스', 2200, 'http://menuImage57.com/150x100.png', 19),
+
+-- Store 20
+(58, '봉골레 파스타', 11400, 'http://menuImage58.com/150x100.png', 20),
+(59, '혼자먹어요 세트', 13900, 'http://menuImage59.com/150x100.png', 20),
+(60, '맵로제', 11000, 'http://menuImage60.com/150x100.png', 20),
+
+-- Store 21
+(61, '명란에그덮밥', 9900, 'http://menuImage61.com/150x100.png', 21),
+(62, '직화삼겹덮밥', 9400, 'http://menuImage62.com/150x100.png', 21),
+(63, '큐브스테이크덮밥', 12400, 'http://menuImage63.com/150x100.png', 21),
+
+-- Store 22
+(64, '족발', 34000, 'http://menuImage64.com/150x100.png', 22),
+(65, '보쌈(대)', 34000, 'http://menuImage65.com/150x100.png', 22),
+(66, '냉채족발(대)', 33000, 'http://menuImage66.com/150x100.png', 22),
+
+-- Store 23
+(67, '오뎅우동', 5000, 'http://menuImage67.com/150x100.png', 23),
+(68, '냉우동', 6000, 'http://menuImage68.com/150x100.png', 23),
+(69, '새우튀김우동', 6500, 'http://menuImage69.com/150x100.png', 23),
+
+-- Store 24
+(70, '등심카츠', 12500, 'http://menuImage70.com/150x100.png', 24),
+(71, '치즈카츠', 13500, 'http://menuImage71.com/150x100.png', 24),
+(72, '안심카츠', 13500, 'http://menuImage72.com/150x100.png', 24),
+
+-- Store 25
+(73, '고기만', 24800, 'http://menuImage73.com/150x100.png', 25),
+(74, '기분좋은 한상', 24800, 'http://menuImage74.com/150x100.png', 25),
+(75, '오롯이 고기만', 15800, 'http://menuImage75.com/150x100.png', 25),
+
+-- Store 26
+(76, '직화 불무뼈닭발', 16000, 'http://menuImage76.com/150x100.png', 26),
+(77, '오돌뼈볶음', 15000, 'http://menuImage77.com/150x100.png', 26),
+(78, '쫄면골뱅이', 15900, 'http://menuImage78.com/150x100.png', 26),
+
+-- Store 27
+(79, '스페셜반반피자', 22900, 'http://menuImage79.com/150x100.png', 27),
+(80, '치폴레치즈후라이피자', 18900, 'http://menuImage80.com/150x100.png', 27),
+(81, '포텐피자', 26900, 'http://menuImage81.com/150x100.png', 27),
+
+-- Store 28
+(82, '킹콩부대찌개', 10000, 'http://menuImage82.com/150x100.png', 28),
+(83, '텍사스부대찌개', 12000, 'http://menuImage83.com/150x100.png', 28),
+(84, '리얼햄가득부대찌개', 12000, 'http://menuImage84.com/150x100.png', 28),
+
+-- Store 29
+(85, '매콤크림 파스타', 10500, 'http://menuImage85.com/150x100.png', 29),
+(86, '봉골레 파스타', 10500, 'http://menuImage86.com/150x100.png', 29),
+(87, '베이컨필라프', 10500, 'http://menuImage87.com/150x100.png', 29),
+
+-- Store 30
+(88, '꿔바로우(소)', 12900, 'http://menuImage88.com/150x100.png', 30),
+(89, '셀프 마라탕', 8500, 'http://menuImage89.com/150x100.png', 30),
+(90, '대표 마라탕', 9900, 'http://menuImage90.com/150x100.png', 30),
+
+-- Store 31
+(91, '가득햄 부대찌개', 18500, 'http://menuImage91.com/150x100.png', 31),
+(92, '모둠 부대찌개', 20900, 'http://menuImage92.com/150x100.png', 31),
+(93, '깡똥햄 부대찌개', 17900, 'http://menuImage93.com/150x100.png', 31),
+
+-- Store 32
+(94, '매콤크림 파스타', 10900, 'http://menuImage94.com/150x100.png', 32),
+(95, '우삼겹 까르보나라', 10900, 'http://menuImage95.com/150x100.png', 32),
+(96, '쉬림프 토마토 파스타', 10900, 'http://menuImage96.com/150x100.png', 32),
+
+-- Store 33
+(97, '짜장면', 6000, 'http://menuImage97.com/150x100.png', 33),
+(98, '짬뽕', 7000, 'http://menuImage98.com/150x100.png', 33),
+(99, '탕수육', 14900, 'http://menuImage99.com/150x100.png', 33),
+
+-- Store 34
+(100, '스페셜샤브', 32000, 'http://menuImage100.com/150x100.png', 34),
+(101, '소고기샤브', 28000, 'http://menuImage101.com/150x100.png', 34),
+(102, '한우샤브', 44000, 'http://menuImage102.com/150x100.png', 34),
+
+-- Store 35
+(103, '소고기 쌀국수', 8500, 'http://menuImage103.com/150x100.png', 35),
+(104, '사이공 볶음밥', 8900, 'http://menuImage104.com/150x100.png', 35),
+(105, '새우볼 6pcs', 5500, 'http://menuImage105.com/150x100.png', 35),
+
+-- Store 36
+(106, '순살 양념구이', 20000, 'http://menuImage106.com/150x100.png', 36),
+(107, '순살 소금구이', 19000, 'http://menuImage107.com/150x100.png', 36),
+(108, '뼈 양념구이', 19000, 'http://menuImage108.com/150x100.png', 36),
+
+-- Store 37
+(109, '국수나무알밥', 8400, 'http://menuImage109.com/150x100.png', 37),
+(110, '소고기낙지덮밥', 9000, 'http://menuImage110.com/150x100.png', 37),
+(111, '잔치국수', 6000, 'http://menuImage111.com/150x100.png', 37),
+
+-- Store 38
+(112, '햄치즈 토스트', 3400, 'http://menuImage112.com/150x100.png', 38),
+(113, '햄스페셜 토스트', 3800, 'http://menuImage113.com/150x100.png', 38),
+(114, '베이컨 포테이토 피자', 5400, 'http://menuImage114.com/150x100.png', 38),
+
+-- Store 39
+(115, '패밀리팩', 32800, 'http://menuImage115.com/150x100.png', 39),
+(116, '불고기박스', 8600, 'http://menuImage116.com/150x100.png', 39),
+(117, '불고기버거세트', 6600, 'http://menuImage117.com/150x100.png', 39),
+
+-- Store 40
+(118, '정성한줄', 4900, 'http://menuImage118.com/150x100.png', 40),
+(119, '국물 떡볶이', 6700, 'http://menuImage119.com/150x100.png', 40),
+(120, '참치마요한줄', 6100, 'http://menuImage120.com/150x100.png', 40),
+
+-- Store 41
+(121, '미스터 에그 샌드위치', 4800, 'http://menuImage121.com/150x100.png', 41),
+(122, '갈릭 베이컨 치즈 세트', 11900, 'http://menuImage122.com/150x100.png', 41),
+(123, '프렌치 토스트', 2200, 'http://menuImage123.com/150x100.png', 41),
+
+-- Store 42
+(124, '얌샘김밥', 3800, 'http://menuImage124.com/150x100.png', 42),
+(125, '라볶이', 6500, 'http://menuImage125.com/150x100.png', 42),
+(126, '참치샐러드김밥', 4800, 'http://menuImage126.com/150x100.png', 42),
+
+-- Store 43
+(127, '아메리카노', 5000, 'http://menuImage127.com/150x100.png', 43),
+(128, '레몬에이드', 5800, 'http://menuImage128.com/150x100.png', 43),
+(129, '허니버터 브레드', 7000, 'http://menuImage129.com/150x100.png', 43),
+
+-- Store 44
+(130, '파인트', 9800, 'http://menuImage130.com/150x100.png', 44),
+(131, '하프갤론', 31500, 'http://menuImage131.com/150x100.png', 44),
+(132, '버라이어티팩', 23400, 'http://menuImage132.com/150x100.png', 44),
+
+-- Store 45
+(133, '초코소라빵', 1900, 'http://menuImage133.com/150x100.png', 45),
+(134, '모카 크림빵', 1900, 'http://menuImage134.com/150x100.png', 45),
+(135, '바닐라라떼', 1200, 'http://menuImage135.com/150x100.png', 45),
+
+-- Store 46
+(136, '빅치킨마요', 4300, 'http://menuImage136.com/150x100.png', 46),
+(137, '진달래', 7800, 'http://menuImage137.com/150x100.png', 46),
+(138, '매화도시락', 10500, 'http://menuImage138.com/150x100.png', 46),
+
+-- Store 47
+(139, '닭갈비철판볶음밥', 7500, 'http://menuImage139.com/150x100.png', 47),
+(140, '유가네 닭갈비 세트', 25000, 'http://menuImage140.com/150x100.png', 47),
+(141, '유가네 모둠세트', 26000, 'http://menuImage141.com/150x100.png', 47),
+
+-- Store 48
+(142, '복숭아아이스티', 3500, 'http://menuImage142.com/150x100.png', 48),
+(143, '메가리카노', 3500, 'http://menuImage143.com/150x100.png', 48),
+(144, '화이트 뱅쇼', 4400, 'http://menuImage144.com/150x100.png', 48),
+
+-- Store 49
+(145, '싸이버거 단품', 4600, 'http://menuImage145.com/150x100.png', 49),
+(146, '케이준떡강정', 4200, 'http://menuImage146.com/150x100.png', 49),
+(147, '화이트갈릭싸이버거 세트', 7500, 'http://menuImage147.com/150x100.png', 49),
+
+-- Store 50
+(148, '설농탕', 10000, 'http://menuImage148.com/150x100.png', 50),
+(149, '고기듬뿍설농탕', 18000, 'http://menuImage149.com/150x100.png', 50),
+(150, '떡만두설농탕', 11000, 'http://menuImage150.com/150x100.png', 50);
